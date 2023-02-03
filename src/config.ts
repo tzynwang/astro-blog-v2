fc8d06e3fe7e3c29d34c0bf40cd36afc17e6e7e1
Tzu Yin <tzyn.wang@gmail.com>
2023-02-03 14:24:19 +0800

    [feat] update global data

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
+export const SITE_TITLE = '普通文組';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const POST_COUNT_IN_INDEX = 10;
