08ddbf759074a96eba5137dd735265bcd7429a3f
Tzu Yin <tzyn.wang@gmail.com>
2023-02-28 16:51:49 +0800

    [feat] tools/post getPostWithCompiledSummary

--- src/tools/post.ts
+++ src/tools/post.ts
@@ -7,3 +7,14 @@ export function sortPostByDate(posts: MarkdownInstance<Post>[]) {
       dayjs(b.frontmatter.date).valueOf() - dayjs(a.frontmatter.date).valueOf()
   );
 }
+
+export function getPostWithCompiledSummary(posts: MarkdownInstance<Post>[]) {
+  return posts.map((post) => {
+    const compiledContent = post.compiledContent();
+    // 在 .md 開頭為 h2+p 的情況下，取得文章第一段內容
+    const summaryWithPTail = compiledContent.split('<p>')[1];
+    // 移除 </p> 以後的內容
+    const summary = summaryWithPTail.split('</p>')[0];
+    return { ...post, summary };
+  });
+}
